b4df9c4d33525d94c03e0c14ef8ffb9a149a47dc
Cristi Paraschiv <cristianv.paraschiv@gmail.com>
2025-12-03 11:19:40 +0200

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Cristi Paraschiv on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
